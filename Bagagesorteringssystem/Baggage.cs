50330f9a676d9703ff91baf11e11ce557980b2d8
GommeS <sandell327@gmail.com>
2021-11-03 20:12:23 +0100

    Add project files.

---
+++ Bagagesorteringssystem/Baggage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bagagesorteringssystem
+{
+    public class Baggage
+    {
+        private int privatenumber;
+
+        private string destination;
+
+        public Baggage()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Bagagesorteringssystem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bagagesorteringssystem
+{
+    class Program
+    {
+        private static string[] destination = new[] { "London", "Berlin", "CPH", "Tokyo", "Hong kung", "Moskva", "Hanoi" };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
